4e027c56625adadf629bab2fb8c09c4790a23dc6
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 16:09:16 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,14 +1,15 @@
 def my_select(collection)
- i = 0
- new_collection =[]
- while i < collection.length
-   if yield collection[i]
-     new_collection << collection[i]
-end
-i +=1
-end
-else puts "Empty collection"
-end
-new_collection
-end
-
+   x = 0
+    new_collection = []
+      if collection.length > 0
+      while i < collection.length
+       if yield(collection[i])
+       new_collection << collection[i]
+     end
+       i+=1
+   end
+ else
+  puts "Empty collection"
+ end
+  new_collection
+  end
